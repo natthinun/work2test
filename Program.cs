007c3e8ad7e1a72f86587cae5e1b58c0da82fabe
natthix <natthinun.blue@mail.kmutt.ac.th>
2021-03-28 23:37:38 +0800

    test no.2

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            x = a;
+            y = b;
+
+            do
+            {
+                if (x == y)
+                {
+                    Console.WriteLine(x); break;
+                }
+                else
+                {
+                    if (x < y)
+                    { x = x + a; } 
+                    else
+                    { y = y + b; }
+                    break;
+                }
+            }
+            while (x == y);
+            Console.WriteLine(x);         
+
+            
+
+           
+        }
+    }
+}
